a764c0f1c13cf58dfd4fcc43bb7828c44c6a90cd
Momelez-Mchunu <50421251+Momelez-Mchunu@users.noreply.github.com>
2021-07-21 02:01:10 +0200

    updated main class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        if (args.length<2 || args.length>2)
+        {
+            
+            System.out.println("Enter number of rows: ");
+            int rows = Integer.parseInt(sc.nextLine());
+            System.out.println("Enter number of columns: ");
+            int columns = Integer.parseInt(sc.nextLine());
+            Board game = new Board(rows,columns);
+            game.drawBoard();
+            String input= "";
+            while (!(input.equals("quit")))
+            {
+                System.out.println("Press enter for new generation or type 'quit' to exit");
+                input = sc.nextLine();
+                if (input.equals(""))
+                {
+                    game.updateBoard();
+                    game.drawBoard();
+                }
+            } 
+        }
+        else  
+        {
+            try {
+                int rows = Integer.parseInt(args[0]);
+                int columns = Integer.parseInt(args[1]);
+                Board game = new Board(rows,columns);
+                game.drawBoard();
+                String input= "";
+                while (!(input.equals("quit")))
+                {
+                    System.out.println("Press enter for new generation or type 'quit' to exit");
+                    input = sc.nextLine();
+                    if (input.equals(""))
+                    {
+                        game.updateBoard();
+                        game.drawBoard();
+                    }
+                }   
+            } catch (Exception e) {
+                System.out.println("Please leave space between your integers");
+            }
+        }
+        sc.close(); 
     }
 }
