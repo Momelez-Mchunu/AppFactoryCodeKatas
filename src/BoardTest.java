79132320e2e4bc3de6a7dc87f894aa828d110713
Momelez-Mchunu <kmchunu029@gmail.com>
2021-07-28 00:29:10 +0200

    updated test file

--- src/BoardTest.java
+++ src/BoardTest.java
@@ -6,27 +6,38 @@ public class BoardTest
     Board board;
     @BeforeEach void initialiseBooard()
     {
-        board = new Board(10,10);
+        board = new Board(4,5);
     }
     @Test void testNumNeighbours()
     {
-        assertEquals(8,board.checkNeighbours(7,4).size());
+        assertEquals(8,board.checkNeighbours(2,3).size());
     }
     @Test void testIsAlive()
     {
-        board.setAlive(7,4);
+        board.setAlive(2,2);
         board.drawBoard();
-        assertTrue(board.accessCell(7, 4).isAlive());
+        assertTrue(board.accessCell(2, 2).isAlive());
 
     }
     @Test void testIsDead()
     {
-        board.setAlive(9,9);
+        board.setAlive(3,4);
         board.drawBoard();
-        board.setDead(9,9);
+        board.setDead(3,4);
         board.drawBoard();
-        assertFalse(board.accessCell(9, 9).isAlive());
+        assertFalse(board.accessCell(3, 4).isAlive());
     }
-    
+    @Test void testUpdateBoard()
+    {
+       board.setAlive(0,2);
+       board.setAlive(1,2);
+       board.setAlive(0,3);
+       board.drawBoard();
+       board.updateBoard();
+       board.drawBoard();
+       assertTrue(board.accessCell(1, 3).isAlive());
+       
+    }
+
 
 }
